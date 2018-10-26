76e63274057f994f9dd90bb9f6d9f1d17594216e
gruntgh <40596510+gruntgh@users.noreply.github.com>
2018-10-26 17:29:42 +0200

    Create MovementBase.cs

---
+++ Ronin.Data/MovementBase.cs
@@ -0,0 +1,6 @@
+public class MovementBase {
+    public double Amount { get; set; }
+    public Date Date { get; set; }
+    public String Type { get; set; }
+    public String Note { get; set; }
+}
